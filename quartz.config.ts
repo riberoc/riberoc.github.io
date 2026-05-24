c4172ea44ec9c2050ff1cff83041c36de1851d1c
marcelo <marcelo.riberocerro@gmail.com>
2026-05-24 17:52:54 +0200

    resting google pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -2,7 +2,7 @@ import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
- * Quartz 4 Configuration
+ * Quartz 4 Configuration. Push
  *
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
